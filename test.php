0682b474f73c044db2cdb189f3c3e3d45460d865
phpkmmt <73811359+phpkmmt@users.noreply.github.com>
2020-11-06 16:51:09 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,20 @@
-for )int i =0l; i <5; i++) {
-  text += "The number is " + i + "<br>";
+<?php
+ 
+function GetPI($diameter){
+ 
+  //円周の長さを求める
+  $pi = $diameter * 3.14;
+ 
+  //計算結果をreturnで返す
+  return $pi;
 }
+ 
+//直径を指定
+$value = 5;
+ 
+//引数に直径の値を指定して結果を取得する
+$sum = GetPI($value);
+ 
+echo '円周の長さ :'.$sum;
+ 
+?>
